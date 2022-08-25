87254a34add9ba1aa1f2cc99fae83f77556f9e50
nickwhitley <nicrofilm@gmail.com>
2022-08-25 19:56:19 -0400

    Styling complete.

--- PhoneDirectory/Models/NewEmployeeModel.cs
+++ PhoneDirectory/Models/NewEmployeeModel.cs
@@ -4,20 +4,22 @@ namespace PhoneDirectory.Models
     public class NewEmployeeModel
     {
         [Required]
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "Name cannot be longer then 20 characters.")]
         public string FirstName { get; set; }
         [Required]
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "Name cannot be longer then 20 characters.")]
         public string LastName { get; set; }
         [Required]
         [MaxLength(20)]
-        [Phone]
+        [RegularExpression(@"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Phone number must in a valid format.")]
         public string PhoneMain { get; set; }
+        [Required]
         [MaxLength(20)]
-        [Phone]
+        [RegularExpression(@"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Phone number must in a valid format.")]
         public string PhoneMobile { get; set; }
         [MinLength(3)]
         [MaxLength(5)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Extension can only contain numbers.")]
         public string Extension { get; set; }
         [MaxLength(300)]
         public string Notes { get; set; }
